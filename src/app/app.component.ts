86111bef3be6a70849627d7b9c60bdbc446e6458
Anup Gupta <anuup22.cs@gmail.com>
2024-12-03 00:21:03 +0530

    add new component with styles and tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MyNewComponentComponent } from "./my-new-component/my-new-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MyNewComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
